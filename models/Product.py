279ac53786b8b88a74513c4d7fbd7f74702a7e50
Antuan01 <joseitox_4@hotmail.com>
2021-03-11 22:15:07 -0400

    feature: product CRUD ready

--- models/Product.py
+++ models/Product.py
@@ -17,6 +17,13 @@ class Product(db.Model):
         db.session.add(self)
         db.session.commit()
 
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 class ProductSchema(ma.Schema):
     class Meta:
         fields = ("id", "name", "price", "stock", "status", "created_at", "updated_at")
--- resources/product.py
+++ resources/product.py
@@ -1,19 +1,25 @@
 from flask_restful import Resource, reqparse
 from models.Product import Product, ProductSchema
 from models.User import User, UserSchema
-
 from flask import request
 
 class ProductResource(Resource):
 
     def get(self, id):
-        return { id: id }
+        return ProductSchema().dump(Product.query.get(id))
 
     def post(self, id):
-        return { "data": request.form }
+        product = Product.query.get(id)
+        product.name = request.form["name"]
+        product.price = request.form["price"]
+        product.stock = request.form["stock"]
+        product.update()
+        return ProductSchema().dump(product), 201
 
     def delete(self, id):
-        return { "action": "delete {}".format(id) }
+        trash = Product.query.get(id)
+        trash.delete()
+        return "", 204 
 
 class ProductList(Resource):
 
@@ -26,5 +32,4 @@ class ProductList(Resource):
                stock=request.form["stock"]
                 )
         data.create()
-
-        return ProductSchema().dump(data)
+        return ProductSchema().dump(data), 201
